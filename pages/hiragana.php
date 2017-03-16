5b3d5e042391666071a276e804d29ed307637024
Valentin Cherchesov <bvdkki@googlemail.com>
2017-03-16 19:00:48 +0100

    hiragana layout

---
+++ pages/hiragana.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="de">
+<?php require '../includes/head.html' ?>
+<body>
+
+<?php require '../includes/title-img.html' ?>
+
+<div class="container">
+
+    <div class="row">
+        <?php require '../includes/sidebar.html' ?>
+        <div class="col-sm-10 content">
+            <h2>Hiragana</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+
+            <div class="learn-container">
+                <div class="before-start">
+
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon left-addon">し</span>
+                        <span class="input-group-addon describing-addon">Gojūon - 46 stimmlose Hiragana</span>
+                        <span class="input-group-btn">
+                            <button class="btn btn-default button-addon" type="button" id="gojuuon-start">Auswählen</button>
+                        </span>
+                    </div>
+
+                    <!--div class="input-group input-group-lg">
+                        <span class="input-group-addon" id="sizing-addon1">じ</span>
+                        <span class="input-group-addon" id="sizing-addon1">Dakuten - 25 stimmhafte Hiragana</span>
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                    </div>
+
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon" id="sizing-addon1">じゃ</span>
+                        <span class="input-group-addon" id="sizing-addon1">Yōon - 36 doppelvokalische Hiragana</span>
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                    </div-->
+
+                </div>
+            </div>
+
+
+            <!--div id="hiragana-container">
+                <div id="info-box-top" class="show-box"></div>
+                <div id="start">
+                    <div id="hiragana-box" class="show-box">
+                    </div>
+                    <div class="show-box">
+                        <input id="hiragana-reading">
+                    </div>
+                    <div class="show-box">
+                        <button id="hiragana-submit" class="jp-button">Submit</button>
+                    </div>
+                </div>
+                <div id="end">
+                    <div class="show-box">
+                        <button id="hiragana-start" class="jp-button">Start</button>
+                    </div>
+                </div>
+                <div id="info-box-bottom" class="show-box"></div>
+            </div-->
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
