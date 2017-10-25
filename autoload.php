603230f3280475f21ce3d1c6881435c05b45e5e5
Valentin Cherchesov <bvdkki@googlemail.com>
2017-10-25 17:59:17 +0200

    got rid of Function.php class and $global use

--- autoload.php
+++ autoload.php
@@ -1,9 +1,8 @@
 <?php
-require 'Classes/Functions.php';
+require 'functions.php';
 require 'Classes/Navigation/Navigation.php';
 require 'Classes/Navigation/Menu.php';
 require 'Classes/Navigation/Item.php';
 
 define('ROOT', __DIR__ . '/');
 define('PAGEROOT', ROOT . 'Views/Pages/');
-$global = new Functions();
---
+++ functions.php
@@ -0,0 +1,65 @@
+<?php
+function getDir()
+{
+    return isset($_GET['dir']) ? $_GET['dir'] : '';
+}
+
+function getPage()
+{
+    return isset($_GET['page']) ? strtolower($_GET['page']) : '';
+}
+
+function getLessonNumber()
+{
+    return isset($_GET['dir']) && isset($_GET['page']) && $_GET['dir'] == 'Lessons' && !empty($_GET['page']) ? $_GET['page'] : '';
+}
+
+function lessonHeader($text)
+{
+    $lesson = getLessonNumber();
+    return '<h1>Lesson ' . $lesson . ' - ' . $text . '</h1>';
+}
+
+function lessonNavigation()
+{
+    $lesson = getLessonNumber();
+
+    switch ($lesson) {
+        case '1':
+            $prev = 'Hitotsu';
+            $next = $lesson + 1;
+            break;
+        case '20':
+            $prev = $lesson - 1;
+            $next = 'Futatsu';
+            break;
+        case '21':
+            $prev = 'Futatsu';
+            $next = $lesson + 1;
+            break;
+        case '40':
+            $prev = $lesson - 1;
+            $next = 'Mittsu';
+            break;
+        default:
+            $prev = $lesson - 1;
+            $next = $lesson + 1;
+    }
+
+    $html = '<div class="button-group">';
+    $html .= '<a class="button prev" href="/Lessons/' . $prev . '">' . (is_int($prev) ? 'Previous' : $prev) . '</a>';
+    $html .= '<a class="button next" href="/Lessons/' . $next . '">' . (is_int($next) ? 'Next' : $next) . '</a>';
+    $html .= '</div>';
+
+    return $html;
+}
+
+function jap($text, $meaning = '', $isParticle = false)
+{
+    return '<jap' . ($isParticle ? ' particle' : '') . '>' . $text . ($meaning ? '<meaning>' . $meaning . '</meaning>' : '') . '</jap>';
+}
+
+function roma($text, $meaning = '')
+{
+    return '<roma>' . $text . ($meaning ? '<meaning>' . $meaning . '</meaning>' : '') . '</roma>';
+}
