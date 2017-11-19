62ac875a6b99b23f354ab8c6d7949e7238b512d1
Alanian <bvdkki@googlemail.com>
2017-11-19 01:41:29 +0100

    kanji index fix

--- Assets/JS/kanji_process.js
+++ Assets/JS/kanji_process.js
@@ -150,10 +150,10 @@ $(document).ready(function () {
                 start(0, 20);
                 break;
             case 'group2':
-                start(20, 20);
+                start(19, 20);
                 break;
             case 'group3':
-                start(40, 20);
+                start(39, 20);
                 break;
         }
     });
