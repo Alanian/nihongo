5038bf0a17dd92402c0a2cd0008474b9089e45f9
Alanian <bvdkki@googlemail.com>
2017-11-19 01:46:32 +0100

    kanji ajax fix

--- Assets/JS/Kanji.class.js
+++ Assets/JS/Kanji.class.js
@@ -27,7 +27,7 @@ KanjiTable.prototype.add = function (characters, reading, meanings) {
 KanjiTable.prototype.fill = function (from, number) {
     var original = this;
     $.ajax({
-        url: 'https://raw.githubusercontent.com/Alanian/nihongo/master/Assets/Data/kanji.txt',
+        url: 'https://raw.githubusercontent.com/Alanian/nihongo/dbf63b5b520da4f8eb5d610c9821dc326400f957/Assets/Data/kanji.txt',
         async: false,
         success: function (data) {
             var lines = data.split('\n');
